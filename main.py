bc1955da7ae1665274cbabbaebe696e2f81915cf
Francisco Cruz <franciscohuertas25@gmail.com>
2025-01-29 11:13:36 -0500

    Refactor functions

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-# Algorithm: Count the number of words in a text string using string splitting
-# Input: A string of text
-# Output: The number of words in the input
-#
-# Step 1: Normalize the text
-# - Strip leading and trailing spaces from the text.
-# - Replace multiple spaces with a single space to avoid empty strings during splitting.
-#
-# Step 2: Split the text into words
-# - Use the space character as a delimiter to split the text into a list of words.
-#
-# Step 3: Count the words
-# - Calculate the length of the list of words.
-#
-# Step 4: Return the word count
-# - Output the total number of words in the text.
-
-def count_words(text: str) -> int:
-    normalized_text = ' '.join(text.split())
-    words = normalized_text.split(' ')
-    word_count = len(words)
-    return word_count
-
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(count_words(file_contents))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text: str) -> int:
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path: str) -> str:
+    with open(path) as f:
+        return f.read()
 
 
 main()
